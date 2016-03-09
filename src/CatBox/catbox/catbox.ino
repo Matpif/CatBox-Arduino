832149af362b3986c63163e351069f676338a1e2
matpif <matthieu.piffeteau@gmail.com>
2016-03-09 21:15:32 +0100

    Add WIFI and led control (led wifi, led power)

--- src/CatBox/catbox/catbox.ino
+++ src/CatBox/catbox/catbox.ino
@@ -3,11 +3,18 @@
  *******************/
 
 #include <Timer.h>
+#include <WiFi.h>
 
 Timer timer;
 
 // pins
 int _pinMotor = 7;
+
+// LEDs controls
+int _pinLEDWifi = 4;
+int _pinLEDPower = 8;
+int _timerLEDWifi = 0;
+int _timerLEDPower = 0;
 
 // Serial return
 String serialString = "";
@@ -16,44 +23,71 @@ String serialString = "";
 long period;
 long periodMotorOn;
 long oneDay;
+long wifiDisconnect;
 
 int nbSequenceByDay = 2; // 2 sequence motor by day
 boolean statMotor = false; // Stop by default
+
+// Wifi params
+char ssid[] = "";
+char pass[] = "";
+int status = WL_IDLE_STATUS;
+int _timerWifi = 0;
 
 // Setup
 void setup() {
 
   Serial.begin(19200);
+  while(!Serial);
+  
+  // Check for the presence of the wifi shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+  }
 
   period = 12L * 60L * 60L * 1000L; // 12 h
   periodMotorOn = 20L * 1000L; // 20 s
   oneDay = 24L * 60L * 60L * 1000L; // 1 day
+  wifiDisconnect = 15L * 1000L; // 15 s
   
   pinMode(_pinMotor, OUTPUT);
+  pinMode(_pinLEDWifi, OUTPUT);
+  pinMode(_pinLEDPower, OUTPUT);
+
+  digitalWrite(_pinLEDPower, HIGH);
   stopMotor();
   
   timer.every(period, startMotor);
   timer.every(oneDay, initSequenceByDay);
+  
+  connectWifi();
+
+  if (WiFi.status() != WL_CONNECTED && WiFi.status() != WL_NO_SHIELD) {
+    _timerWifi = timer.every(wifiDisconnect, connectWifi);
+  }
 }
 
 // Main programme
 void loop() {
   timer.update();
+  //ledController(); // TODO: Problème plantage timer
 }
 
 // Start Motor since period periodMotorOn
 void startMotor() {
-
+  
+  Serial.println("Start Motor");
   if (nbSequenceByDay > 0 && !statMotor) {
+    timer.after(periodMotorOn, stopMotor);
     statMotor = true;
     nbSequenceByDay--;
     digitalWrite(_pinMotor, HIGH);
-    timer.after(periodMotorOn, stopMotor);
   }
 }
 
 // Stop Motor
 void stopMotor() {
+  Serial.println("Stop Motor");
   statMotor = false;
   digitalWrite(_pinMotor, LOW);
 }
@@ -97,6 +131,87 @@ void serialEvent(){
           startMotor();
         }
       }
+    }
+  }
+}
+
+void ledController() {
+
+  switch (WiFi.status()) {
+    case WL_CONNECTION_LOST:
+    case WL_CONNECT_FAILED:
+
+      if (_timerLEDWifi == 0)
+        _timerLEDWifi = timer.every(1000L, blinkLEDWifi);
+
+      if (_timerWifi == 0)
+        _timerWifi = timer.every(wifiDisconnect, connectWifi);
+        
+      break;
+    case  WL_CONNECTED:
+    
+      if (_timerLEDWifi != 0)
+        timer.stop(_timerLEDWifi);
+      
+      digitalWrite(_pinLEDWifi, LOW);
+      _timerLEDWifi=0;
+      
+      break;
+    default:
+    
+      if (_timerLEDWifi != 0)
+        timer.stop(_timerLEDWifi);
+        
+      digitalWrite(_pinLEDWifi, HIGH);
+      _timerLEDWifi=0;
+    break;
+  }
+}
+
+void blinkLEDWifi() {
+  digitalWrite(_pinLEDWifi, !digitalRead(_pinLEDWifi));
+}
+
+void connectWifi() {
+
+  Serial.println("Connecting...");
+  if (WiFi.status() != WL_CONNECTED && WiFi.status() != WL_NO_SHIELD) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+    
+    if (WiFi.status() == WL_CONNECTED) {
+      
+      Serial.print("IP Address: ");
+      Serial.println(WiFi.localIP());
+      
+      if (_timerWifi != 0) {
+        timer.stop(_timerWifi);
+        _timerWifi = 0;
+      }
+      
+    } else {
+      
+      Serial.println("Disconnected");
+      if (_timerWifi == 0 && WiFi.status() != WL_NO_SHIELD) {
+        _timerWifi = timer.every(wifiDisconnect, connectWifi);
+      } else if (_timerWifi != 0 && WiFi.status() == WL_NO_SHIELD) {
+        timer.stop(_timerWifi);
+        _timerWifi = 0;
+      }
+    }
+  } else {
+    
+    Serial.println("Disconnected");
+    if (_timerWifi == 0 && WiFi.status() != WL_NO_SHIELD) {
+      _timerWifi = timer.every(wifiDisconnect, connectWifi);
+    } else if (_timerWifi != 0 && WiFi.status() == WL_NO_SHIELD) {
+      timer.stop(_timerWifi);
+      _timerWifi = 0;
     }
   }
 }
